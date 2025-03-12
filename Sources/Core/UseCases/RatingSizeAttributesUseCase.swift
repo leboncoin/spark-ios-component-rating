a5acc9c4efc96670ab838f3a5eb6f473f48aefe8
Robin Lemaire <robin.lemaire@adevinta.com>
2025-03-12 17:45:44 +0100

    [Migration] Replace Adevinta to Leboncoin

--- Sources/Core/UseCases/RatingSizeAttributesUseCase.swift
+++ Sources/Core/UseCases/RatingSizeAttributesUseCase.swift
@@ -3,7 +3,7 @@
 //  SparkRating
 //
 //  Created by Michael Zimmermann on 20.11.23.
-//  Copyright © 2023 Adevinta. All rights reserved.
+//  Copyright © 2023 Leboncoin. All rights reserved.
 //
 
 import Foundation
